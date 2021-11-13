08719ca6df5dda109c5b436a989d95c594b7ba12
Anton Golub <antongolub@antongolub.com>
2021-11-13 19:16:05 +0300

    build: fix flow build

--- flow-typed/ajv-formats.js
+++ flow-typed/ajv-formats.js
@@ -1,5 +1,5 @@
 declare module 'ajv-formats' {
-  import type Ajv from 'ajv'
+  // import type Ajv from 'ajv';
 
   declare type FormatName = 'date' | 'time' | 'date-time' | 'duration' | 'uri' | 'uri-reference' | 'uri-template' | 'url' | 'email' | 'hostname' | 'ipv4' | 'ipv6' | 'regex' | 'uuid' | 'json-pointer' | 'json-pointer-uri-fragment' | 'relative-json-pointer' | 'byte' | 'int32' | 'int64' | 'float' | 'double' | 'password' | 'binary'
   declare type FormatMode = 'fast' | 'full'
@@ -11,5 +11,5 @@ declare module 'ajv-formats' {
 
   declare type FormatsPluginOptions = FormatName[] | FormatOptions
 
-  declare module.exports: (ajv: Ajv, opts?: FormatsPluginOptions) => Ajv
+  declare module.exports: <T = any>(ajv: T, opts?: FormatsPluginOptions) => T
 }
